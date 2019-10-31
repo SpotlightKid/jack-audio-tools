e2c00f0b2b0831ef490c885ededd62496a24e0e0
Christopher Arndt <chris@chrisarndt.de>
2019-10-31 17:04:37 +0100

    Add simple script search LV2 plugins by regex
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

---
+++ lv2/lv2grep.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import argparse
+import re
+import sys
+
+import lilv
+
+
+def main(args=None):
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-i', '--ignore-case', action="store_true",
+                    help="Ignore case")
+    ap.add_argument('pattern', help="LV2 plugin URI pattern")
+
+    args = ap.parse_args(args)
+    rx = re.compile(args.pattern, re.I if args.ignore_case else 0)
+    world = lilv.World()
+    world.load_all()
+
+    for node in world.get_all_plugins():
+        uri = str(node.get_uri())
+
+        if rx.search(uri):
+            print(uri)
+
+
+if __name__ == '__main__':
+    sys.exit(main() or 0)
