99dcd8135acb910e95d01538f26da28fbcac8d78
Christopher Arndt <chris@chrisarndt.de>
2019-11-14 01:32:42 +0100

    Clean-up of lv2_list_plugin_presets script
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- lv2/lv2_list_plugin_presets.py
+++ lv2/lv2_list_plugin_presets.py
@@ -6,7 +6,28 @@ import sys
 import lilv
 
 
-NS_PRESET = 'http://lv2plug.in/ns/ext/presets#'
+NS_PRESETS = 'http://lv2plug.in/ns/ext/presets#'
+
+
+def print_presets(world, plugin):
+    presets = plugin.get_related(world.ns.presets.Preset)
+    preset_list = []
+
+    for preset in presets:
+        world.load_resource(preset)
+        labels = world.find_nodes(preset, world.ns.rdfs.label, None)
+
+        if labels:
+            label = str(labels[0])
+        else:
+            label = None
+            print("Preset '%s' has no rdfs:label" % preset, file=sys.stderr)
+
+        preset_list.append((label, str(preset)))
+
+    for label, preset_uri in sorted(preset_list, key=lambda x: x[0] or ''):
+        print("Label: %s" % label or "")
+        print("URI: %s\n" % preset_uri)
 
 
 def main(args=None):
@@ -15,35 +36,21 @@ def main(args=None):
     if args:
         uri = args[0]
     else:
-        return "Usage: lv2-list-plugin-presets <plugin URI>"
+        return "Usage: lv2_list_plugin_presets <plugin URI>"
 
     world = lilv.World()
     world.load_all()
-    preset_ns = lilv.Namespace(world, NS_PRESET)
+    world.ns.presets = lilv.Namespace(world, NS_PRESETS)
     plugins = world.get_all_plugins()
-    plugin_uri = world.new_uri(uri)
 
-    if plugin_uri is None or plugin_uri not in plugins:
-        return "Plugin with URI '%s' not found" % uri
+    try:
+        plugin = plugins[uri]
+    except KeyError as exc:
+        return "error: no plugin with URI '%s' found." % uri
+    except ValueError as exc:
+        return "error: %s" % exc
 
-    plugin = plugins[plugin_uri]
-    presets = plugin.get_related(preset_ns.Preset)
-
-    preset_list = []
-
-    for preset in presets:
-        labels = world.find_nodes(preset, world.ns.rdfs.label, None)
-
-        if labels:
-            label = str(labels[0])
-        else:
-            label = str(preset)
-            print("Preset '%s' has no rdfs:label" % preset, file=sys.stderr)
-
-        preset_list.append((label, str(preset)))
-
-    for preset in sorted(preset_list):
-        print("%s: %s" % preset)
+    print_presets(world, plugin)
 
 
 if __name__ == '__main__':
